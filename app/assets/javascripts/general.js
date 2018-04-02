c27d43b66c4d2e731fad457c1518815b2f81d3a2
samro <samuel.rossi@villedupre.fr>
2018-04-03 00:22:46 +0200

    encore un truc bizarre de git

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,12 +1,11 @@
-function coucou() {
-	alert("coucou");
-}
 
 function animation_menus_principaux() {
 	$("#bouton_menu_gauche").click(function(event){
+		$("main, #navbarleft").css("transition", "left .4s");
 		$("main, #navbarleft").toggleClass('menu_gauche_masque'); //toggleclass ajoute ou enlève une classe selon qu'elle est déjà ou non présente
 	});
 	$("#bouton_menu_droit").click(function(event){
+		$("main, #navbarright").css("transition", "right .4s");
 		$("main, #navbarright").toggleClass('menu_droit_masque'); //toggleclass ajoute ou enlève une classe selon qu'elle est déjà ou non présente
 	});
 }
